115d50e608b106c7505e7674db144c96845273d9
joseeden <josemanuelitoeden@gmail.com>
2026-01-11 18:39:57 +0800

    Add controller and view for test blog app

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :articles, only: [:show]
 end
