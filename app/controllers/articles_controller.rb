0c42ea775902d585045f7c640a246acc2bc17719
joseeden <josemanuelitoeden@gmail.com>
2026-01-11 23:03:01 +0800

    Add validation, create and save actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+## app/controllers/articles_controller.rb
 class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
@@ -6,4 +7,43 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
+
+## app/controllers/articles_controller.rb
+# class ArticlesController < ApplicationController
+#   def show
+#     @article = Article.find(params[:id])
+#   end
+
+#   def index
+#     @articles = Article.all
+#   end
+
+#   def new
+#   end
+
+#   def create
+#     @article = Article.new(article_params)
+
+#     if @article.save
+#       redirect_to @article
+#     else
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def article_params
+#     params.require(:article).permit(:title, :description)
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# config/routes.rb
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
